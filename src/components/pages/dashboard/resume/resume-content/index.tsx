946320a70aaa8729b301dcd5255992b88f3df2e4
C1 <cleanthobeltrao@gmail.com>
2026-09-25 15:00:20 -0300

    criação do elemento arrastável que conterá o currículo

--- src/components/pages/dashboard/resume/resume-content/index.tsx
+++ src/components/pages/dashboard/resume/resume-content/index.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { TransformControls } from "./controls";
+import { NavigationHeader } from "./header";
+import { ResumeTemplate } from "./templates";
+
 export const ResumeContent = () => {
   return (
-    <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background"></section>
+    <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background">
+      <TransformWrapper
+        initialScale={0.5}
+        minScale={0.4}
+        centerOnInit
+        centerZoomedOut
+        limitToBounds={false}
+      >
+        <>
+          <NavigationHeader />
+          <TransformControls />
+          <TransformComponent>
+            <ResumeTemplate />
+          </TransformComponent>
+        </>
+      </TransformWrapper>
+    </section>
   );
 };
