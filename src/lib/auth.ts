14d246292db75c09cac55ed9f99987d7eb67733a
C1 <cleanthobeltrao@gmail.com>
2026-09-26 13:09:17 -0300

    login sendo persistido no banco de dados

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db/drizzle";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub, Google],
   pages: {
-    newUser: "/",
     signIn: "/auth/login",
   },
   callbacks: {
